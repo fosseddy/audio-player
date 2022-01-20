34a4929dff247ed3816f6a3067ceb0bd3696a0ef
Mark Fosseddy <markfosseddy@gmail.com>
2022-01-20 21:22:04 +0500

    add simple controls

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("hello, world");
+const playBtn = document.querySelector("#play-btn");
+const pauseBtn = document.querySelector("#pause-btn");
+const resetBtn = document.querySelector("#reset-btn");
+
+const audio = new Audio("basta-war.mp3");
+
+playBtn.addEventListener("click", () => {
+    audio.play();
+});
+
+pauseBtn.addEventListener("click", () => {
+    audio.pause();
+});
+
+resetBtn.addEventListener("click", () => {
+    audio.load();
+});

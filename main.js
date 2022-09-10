7a613b03a72bd78f4cb90225be57f3030b645000
Mark Fosseddy <markfosseddy@gmail.com>
2022-09-10 18:42:49 +0500

    load songs

--- main.js
+++ main.js
@@ -1,11 +1,89 @@
+function assert(cond, msg = null) {
+  if (!cond) {
+    throw new Error(`Assertion failed${msg ? ": " + msg : "."}`);
+  }
+}
+
+const state = {
+  songs: [],
+  currSong: 0 // first song by default
+};
+
+window.state = state;
+
 document.querySelector("input[type='file']").addEventListener("change", e => {
   const { files } = e.target;
+  if (!files.length) return;
 
-  if (files.length) {
-    for (const f of files) {
-      console.log(f);
+  // @NOTE(art): save for later use in event handler, because we lose it
+  // after reset target value in the end of the function
+  const filesLen = files.length;
+  const tmp = [];
+  let errcount = 0;
+
+  for (const f of files) {
+    const s = {
+      name: f.name,
+      audio: new Audio(URL.createObjectURL(f))
     }
 
-    e.target.value = "";
+    s.audio.addEventListener("canplaythrough", () => {
+      tmp.push(s);
+
+      // @NOTE(art): save files only when all are loaded
+      // @TODO(art): show some loading indicator to user?
+      // @TODO(art): songs are saved out of order
+      if (tmp.length === filesLen - errcount) {
+        state.songs.push(...tmp);
+        drawSongList();
+      }
+    });
+
+    s.audio.addEventListener("error", () => {
+      // @TODO(art): show error to user
+      console.error(s.audio.error);
+      URL.revokeObjectURL(s.audio.src);
+      errcount++;
+    });
   }
+
+  e.target.value = "";
 });
+
+const songList = document.querySelector("#song-list");
+assert(songList != null);
+
+function drawSongList() {
+  clearChildren(songList);
+
+  const items = [];
+  for (let i = 0; i < state.songs.length; i++) {
+    items.push(createSongListItem(i));
+  }
+
+  songList.append(...items);
+}
+
+function createSongListItem(idx) {
+  const li = document.createElement("li");
+  li.textContent = state.songs[idx].name;
+
+  li.style.padding = "1rem";
+
+  if (state.currSong === idx) {
+    li.style.background = "red";
+  }
+
+  li.addEventListener("click", () => {
+    state.currSong = idx;
+    drawSongList();
+  });
+
+  return li;
+}
+
+function clearChildren(container) {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+}
